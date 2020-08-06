453ebb9315b1ea193965104a27e9069ef9d0cd3d
RostikDEP <54043052+RostikDEP@users.noreply.github.com>
2020-08-06 05:50:34 -0400

    Add files via upload

---
+++ mainScript.js
@@ -0,0 +1,9 @@
+burger.onclick = function () {
+	var burgerVar = document.getElementById('burgerMenu');
+	if (burgerVar.style.display ==='none') {
+		burgerVar.style.display = 'block';
+	}else{
+		burgerVar.style.display = 'none';
+	}
+}
+
